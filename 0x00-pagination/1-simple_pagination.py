6390cdfeaee9961584f8b0427da4f2f15148e8ed
kariaki58 <programmerjavascript901@gmail.com>
2024-04-01 13:49:33 +0100

    implement a method names get_page that returns a dataset based on page number and pagesize

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,11 @@ import math
 from typing import List
 
 
-
+def index_range(page: int, page_size: int) -> tuple:
+    """index range function that calculates the page number"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
@@ -15,13 +19,6 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
-    
-    @staticmethod
-    def index_range(page: int, page_size: int) -> tuple:
-        """index range function that calculates the page number"""
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
     
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -34,10 +31,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        start_page, end_page = self.index_range(page, page_size)
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
-
-        dataset = self.dataset()[start_page:end_page]
+        start_page, end_page = index_range(page, page_size)
+        dataset = self.dataset()[start_page: end_page]
         return dataset
-        
