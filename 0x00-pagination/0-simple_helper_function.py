31d78955531ff5bc2b289ee1ecbbc4411cbdd724
kariaki58 <programmerjavascript901@gmail.com>
2024-04-01 13:20:48 +0100

    fixed pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,4 @@ def index_range(page: int, page_size: int) -> tuple:
     """index range function that calculates the page number"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    
     return (start_index, end_index)
