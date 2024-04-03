6fa2639b40311c871ff74e08a9a77c26fb01f5b3
kariaki58 <programmerjavascript901@gmail.com>
2024-04-03 16:49:48 +0100

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""fifo caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Basic Caching class"""
+    def __init__(self):
+        """initailization function, get cache_data from the base class"""
+        self.used = []
+        super().__init__()
+
+    def put(self, key, item):
+        """insert a dictionary data to the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.used.pop()
+                del self.cache_data[removed]
+                print("DISCARD: {}".format(removed))
+            self.cache_data[key] = item
+            self.used.append(key)
+        else:
+            return None
+
+    def get(self, key):
+        """get data from the cache"""
+        if key is not None:
+            if key in self.cache_data:
+                self.used.remove(key)
+                self.used.append(key)
+                return self.cache_data[key]
+        return None
